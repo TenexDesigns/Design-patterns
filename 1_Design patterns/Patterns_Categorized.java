3a88d02d5935fecad67e5d40f8a6f84d3aafc848
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-21 15:41:30 +0300

    Create Patterns_Categorized.java

---
+++ 1_Design patterns/Patterns_Categorized.java
@@ -0,0 +1,40 @@
+Certainly! Here's a list of the 23 classic design patterns categorized into the three groups: Creational, Structural, and Behavioral, as introduced by the Gang of Four:
+
+**Creational Design Patterns:**
+These patterns deal with object creation mechanisms, trying to create objects in a manner suitable to the situation.
+
+1. **Factory Method**: Provides an interface for creating objects but allows subclasses to alter the type of objects that will be created.
+2. **Abstract Factory**: Provides an interface for creating families of related or dependent objects without specifying their concrete classes.
+3. **Builder**: Separates the construction of a complex object from its representation, allowing the same construction process to create different representations.
+4. **Prototype**: Creates new objects by copying an existing object's properties, avoiding the need to create new instances from scratch.
+5. **Singleton**: Ensures that a class has only one instance and provides a global point of access to that instance.
+
+**Structural Design Patterns:**
+These patterns deal with the composition of classes and objects to form larger structures.
+
+6. **Adapter**: Converts the interface of one class into another interface clients expect. Allows classes with incompatible interfaces to work together.
+7. **Bridge**: Decouples an abstraction from its implementation, allowing both to vary independently.
+8. **Composite**: Composes objects into tree structures to represent part-whole hierarchies. Clients can treat individual objects and compositions of objects uniformly.
+9. **Decorator**: Attaches additional responsibilities to an object dynamically. Provides a flexible alternative to subclassing for extending functionality.
+10. **Facade**: Provides a simplified interface to a complex subsystem, making it easier to use and understand.
+11. **Flyweight**: Shares objects to reduce memory usage by reusing common parts of the object across multiple instances.
+12. **Proxy**: Provides a surrogate or placeholder for another object to control access, add behavior, or defer instantiation.
+
+**Behavioral Design Patterns:**
+These patterns deal with object collaboration, responsibilities, and algorithms.
+
+13. **Chain of Responsibility**: Creates a chain of handler objects for a request, allowing the request to be handled by different handlers in the chain.
+14. **Command**: Turns a request into a stand-alone object that contains all information about the request. Allows parameterization of clients with different requests.
+15. **Interpreter**: Defines a grammar for a language and an interpreter to interpret the language expressions.
+16. **Iterator**: Provides a way to access elements of a collection without exposing its underlying representation. Facilitates traversal of collections.
+17. **Mediator**: Defines a central mediator object that encapsulates communication between objects. Promotes loose coupling between objects.
+18. **Memento**: Captures an object's internal state and saves it externally so that the object can be restored to that state later.
+19. **Observer**: Defines a dependency between objects, so when one object changes state, all its dependents are notified and updated automatically.
+20. **State**: Allows an object to change its behavior when its internal state changes. Encapsulates different behaviors as separate classes.
+21. **Strategy**: Defines a family of interchangeable algorithms and lets them be used interchangeably within a context.
+22. **Template Method**: Defines the structure of an algorithm, allowing subclasses to provide specific implementations for certain steps.
+23. **Visitor**: Separates an algorithm from an object structure on which it operates. Allows adding new operations to objects without modifying their classes.
+
+These 23 design patterns collectively form a comprehensive set of solutions to recurring design problems in software development. Each pattern addresses a specific concern, and by understanding and applying these patterns appropriately, developers can create more maintainable, flexible, and modular software systems.
+
+
