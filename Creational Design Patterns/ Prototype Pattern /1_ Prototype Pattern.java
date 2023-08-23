c73941fc6a05958cb7abc3503f55d370b5f310e7
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-23 17:36:03 +0300

    Create 1_ Prototype Pattern.java

---
+++ Creational Design Patterns/ Prototype Pattern /1_ Prototype Pattern.java
@@ -0,0 +1,81 @@
+The Prototype Pattern is a creational design pattern that allows you to create copies of existing objects (prototypes) without making the code dependent on their concrete classes. It's useful when you want to create objects based on existing instances, especially when object creation is expensive or when objects have complex configurations.
+
+**Key Concepts:**
+
+1. **Prototype:** An interface or abstract class that declares a method for cloning itself. Concrete prototypes implement this method to create copies.
+
+2. **Concrete Prototypes:** Implementations of the prototype interface that provide cloning behavior. These are the objects you can clone.
+
+3. **Client:** The code that requests clones of the prototypes. It doesn't need to know the specific classes of the prototypes.
+
+**Example of Prototype Pattern:**
+
+Let's say we're building a graphic design application, and we want to create copies of graphical objects such as circles and rectangles. Here's how the Prototype Pattern can be applied:
+
+```java
+// Prototype (Interface)
+interface Graphic {
+    Graphic clone();
+    void draw();
+}
+
+// Concrete Prototypes
+class Circle implements Graphic {
+    private int radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+
+    public Graphic clone() {
+        return new Circle(radius);
+    }
+
+    public void draw() {
+        System.out.println("Drawing a circle with radius " + radius);
+    }
+}
+
+class Rectangle implements Graphic {
+    private int width;
+    private int height;
+
+    public Rectangle(int width, int height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public Graphic clone() {
+        return new Rectangle(width, height);
+    }
+
+    public void draw() {
+        System.out.println("Drawing a rectangle with width " + width + " and height " + height);
+    }
+}
+
+// Client Code
+public class Client {
+    public static void main(String[] args) {
+        Graphic originalCircle = new Circle(5);
+        Graphic clonedCircle = originalCircle.clone();
+
+        Graphic originalRectangle = new Rectangle(10, 7);
+        Graphic clonedRectangle = originalRectangle.clone();
+
+        originalCircle.draw();
+        clonedCircle.draw();
+
+        originalRectangle.draw();
+        clonedRectangle.draw();
+    }
+}
+```
+
+In this example, the Prototype Pattern allows you to create copies of `Circle` and `Rectangle` objects by using the `clone()` method. This way, you can create new instances with the same characteristics as the original objects without knowing their specific classes.
+
+The Prototype Pattern is beneficial when object creation is resource-intensive or when you want to avoid tight coupling between client code and concrete classes. It's particularly useful in scenarios where you need to create objects with varying configurations or where instances can be reused.
+
+
+
+  
