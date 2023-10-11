9b84cb085c13f7b33abc43bdaba50b55010866d9
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-10-11 07:55:54 +0300

    Create 2_Most_Important_Patterns.java

---
+++ Most_Important_Patterns/2_Most_Important_Patterns.java
@@ -0,0 +1,33 @@
+Design patterns offer reusable solutions to common problems in software development. These patterns are not code per se, but rather serve as a template or guide on how to solve a problem. Design patterns are particularly beneficial for object-oriented programming languages, and they can significantly enhance the readability and maintainability of your code. 
+
+The 23 classic design patterns can be categorized into three types: creational, structural, and behavioral design patterns [Source 0](https://www.netsolutions.com/insights/software-design-pattern/). 
+
+Out of these, here are some of the most commonly used design patterns:
+
+1. **Factory Method (Creational)**: This pattern provides an interface for creating objects in a superclass, but allows subclasses to alter the type of objects that will be created.
+
+2. **Singleton (Creational)**: The Singleton pattern ensures that a class has only one instance, and provides a global access point to it.
+
+3. **Adapter (Structural)**: This pattern allows objects with incompatible interfaces to collaborate by wrapping its own interface around that of an already existing class.
+
+4. **Decorator (Structural)**: This is a structural design pattern that lets you attach new behaviors to objects by placing these objects inside special wrapper objects that contain the behaviors.
+
+5. **Observer (Behavioral)**: This pattern defines a subscription mechanism to notify multiple objects about any events that happen to the object they’re observing.
+
+6. **Strategy (Behavioral)**: This pattern lets you define a family of algorithms, put each of them into a separate class, and make their objects interchangeable.
+
+7. **Facade (Structural)**: This pattern provides a simplified interface to a complex subsystem. It doesn't hide the subsystem's complexity but makes the subsystem easier to use by reducing its public interface.
+
+8. **Command (Behavioral)**: This pattern turns a request into a stand-alone object that contains all information about the request. This transformation lets you parameterize methods with different requests, delay or queue a request's execution, and support undoable operations.
+
+9. **Builder (Creational)**: This pattern lets you construct complex objects step by step. The pattern allows you to produce different types and representations of an object using the same construction code.
+
+These patterns are not only widely used in software development but also form a common language for developers. Understanding these patterns can significantly improve your ability to communicate with other developers and to design software more efficiently.
+
+However, it's important to remember that design patterns are not a silver bullet, and they may not be suitable for every situation. The decision to use a specific pattern should be based on whether it provides a practical and efficient solution for your specific problem [Source 0](https://www.netsolutions.com/insights/software-design-pattern/).
+
+
+
+
+
+.....
